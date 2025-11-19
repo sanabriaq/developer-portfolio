01d7c0029bb16dcc99b1efd255f4b3ade1baad8a
sanabriaq <sanabriajoaopedro15@gmail.com>
2025-11-18 22:22:46 -0300

    start project

---
+++ portfólio/script.js
@@ -0,0 +1,12 @@
+let avaliacao = 200
+if(avaliacao ==1) {
+    console.log ('Mau resultado')
+} else if (avaliacao ==2){
+   console.log('Resultado negativo') 
+} else if (avaliacao ==3){
+    console.log('Resultado positivo')
+} else if (avaliacao ==4){
+    console.log('Resultado bom')
+} else {
+    console.log('Resultado excelente')
+}
